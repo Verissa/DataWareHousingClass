e38ce26a94ecaeb633547f868474c295e34c3b54
Verrisa Marian Owusu <38285965+Verissa@users.noreply.github.com>
2023-12-07 11:56:55 -0500

    add taxi_trips_ending_at_airport.sql

--- nyc_transit/analyses/taxi_trips_ending_at_airport.sql
+++ nyc_transit/analyses/taxi_trips_ending_at_airport.sql
@@ -1,14 +1,6 @@
-SELECT
-    COUNT(*) as total_trips
-FROM
-    mart__fact_all_taxi_trips
-WHERE
-    dropOff_locationID IN (
-        SELECT
-            LocationID
-        FROM
-            mart__dim_locations
-        WHERE
-            service_zone IN ('Airports', 'EWR')
-    );
+select count(*) as trips
+from {{ ref('mart__fact_all_taxi_trips') }} t
+join {{ ref('mart__dim_locations') }} dl on t.DOlocationID = dl.LocationID
+where dl.service_zone in ('Airports', 'EWR')
+group by all
     
